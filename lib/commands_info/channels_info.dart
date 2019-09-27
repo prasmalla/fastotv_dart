88c3f634d2135c54bf41c6b26816a3f6bfb5ab20
topilski <atopilski@gmail.com>
2019-09-27 16:06:55 +0300

    UI type

--- lib/commands_info/channels_info.dart
+++ lib/commands_info/channels_info.dart
@@ -3,15 +3,18 @@ import 'package:fastotv_dart/commands_info/epg_info.dart';
 class ChannelInfo {
   final String id;
   final int type;
+  final int ui_type;
   final EpgInfo epg;
   final bool video;
   final bool audio;
 
-  ChannelInfo(this.id, this.type, this.epg, this.video, this.audio);
+  ChannelInfo(
+      this.id, this.type, this.ui_type, this.epg, this.video, this.audio);
 
   ChannelInfo.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         type = json['type'],
+        ui_type = json['ui_type'],
         epg = EpgInfo.fromJson(json['epg']),
         video = json['video'],
         audio = json['audio'];
@@ -19,6 +22,7 @@ class ChannelInfo {
   Map<String, dynamic> toJson() => {
         'id': id,
         'type': type,
+        'ui_type': ui_type,
         'epg': epg.toJson(),
         'video': video,
         'audio': audio
