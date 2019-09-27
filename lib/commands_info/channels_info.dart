42c802c80d4a94e468b88daeb598a385cf2951b4
topilski <atopilski@gmail.com>
2019-09-27 10:30:25 +0300

    Fix build

--- lib/commands_info/channels_info.dart
+++ lib/commands_info/channels_info.dart
@@ -1,24 +1,31 @@
 import 'package:fastotv_dart/commands_info/epg_info.dart';
 
+class Type {
+  static const PUBLIC = 0;
+  static const PRIVATE = 1;
+}
+
 class ChannelInfo {
-  enum Type { 
-    PUBLIC = 0,
-    PRIVATE = 1
-  }
   final String id;
   final Type type;
   final EpgInfo epg;
   final bool video;
   final bool audio;
 
-  ChannelInfo(this.id, this.epg, this.video, this.audio);
+  ChannelInfo(this.id, this.type, this.epg, this.video, this.audio);
 
   ChannelInfo.fromJson(Map<String, dynamic> json)
       : id = json['id'],
+        type = json['type'],
         epg = EpgInfo.fromJson(json['epg']),
         video = json['video'],
         audio = json['audio'];
 
-  Map<String, dynamic> toJson() =>
-      {'id': id, 'type': type,'epg': epg.toJson(), 'video': video, 'audio': audio};
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'type': type,
+        'epg': epg.toJson(),
+        'video': video,
+        'audio': audio
+      };
 }
