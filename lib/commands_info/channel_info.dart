0ea5c3213315da90ffc1ad8ab0215b96a16cd653
topilski <atopilski@gmail.com>
2019-10-02 14:05:59 +0300

    Tags and group for channel

--- lib/commands_info/channel_info.dart
+++ lib/commands_info/channel_info.dart
@@ -4,27 +4,33 @@ class ChannelInfo {
   final String id;
   final int type;
   final int ui_type;
+  final String group;
+  final List<String> tags;
   final EpgInfo epg;
   final bool video;
   final bool audio;
 
-  ChannelInfo(
-      this.id, this.type, this.ui_type, this.epg, this.video, this.audio);
+  ChannelInfo(this.id, this.type, this.ui_type, this.group, this.tags, this.epg,
+      this.video, this.audio);
 
   factory ChannelInfo.fromJson(Map<String, dynamic> json) {
     final id = json['id'];
     final type = json['type'];
     final ui_type = json['ui_type'];
+    final group = json['group'];
+    final tags = json['tags'];
     final epg = EpgInfo.fromJson(json['epg']);
     final video = json['video'];
     final audio = json['audio'];
-    return ChannelInfo(id, type, ui_type, epg, video, audio);
+    return ChannelInfo(id, type, ui_type, group, tags, epg, video, audio);
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'type': type,
         'ui_type': ui_type,
+        'group': group,
+        'tags': tags,
         'epg': epg.toJson(),
         'video': video,
         'audio': audio
