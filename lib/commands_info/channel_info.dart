b808392e33671f6aef2892102c5c5178b237912c
topilski <atopilski@gmail.com>
2019-10-03 18:39:12 +0300

    Tags remove

--- lib/commands_info/channel_info.dart
+++ lib/commands_info/channel_info.dart
@@ -5,24 +5,22 @@ class ChannelInfo {
   final int type;
   final int stream_type;
   final String group;
-  final List<String> tags;
   final EpgInfo epg;
   final bool video;
   final bool audio;
 
-  ChannelInfo(this.id, this.type, this.stream_type, this.group, this.tags,
-      this.epg, this.video, this.audio);
+  ChannelInfo(this.id, this.type, this.stream_type, this.group, this.epg,
+      this.video, this.audio);
 
   factory ChannelInfo.fromJson(Map<String, dynamic> json) {
     final id = json['id'];
     final type = json['type'];
     final stream_type = json['stream_type'];
     final group = json['group'];
-    final List<String> tags = json['tags'].cast<String>();
     final epg = EpgInfo.fromJson(json['epg']);
     final video = json['video'];
     final audio = json['audio'];
-    return ChannelInfo(id, type, stream_type, group, tags, epg, video, audio);
+    return ChannelInfo(id, type, stream_type, group, epg, video, audio);
   }
 
   Map<String, dynamic> toJson() => {
@@ -30,7 +28,6 @@ class ChannelInfo {
         'type': type,
         'stream_type': stream_type,
         'group': group,
-        'tags': tags,
         'epg': epg.toJson(),
         'video': video,
         'audio': audio
