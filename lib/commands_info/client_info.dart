b2a01028da0b4f2b14b3f112630c294ffdadb9fb
topilski <atopilski@gmail.com>
2019-10-03 06:11:39 +0300

    ClientInfo device id

--- lib/commands_info/client_info.dart
+++ lib/commands_info/client_info.dart
@@ -29,22 +29,26 @@ class OperationSystemInfo {
 
 class ClientInfo {
   final String login;
+  final String device_id;
   final OperationSystemInfo os;
   final String cpu_brand;
   final int bandwidth;
 
-  ClientInfo(this.login, this.os, this.cpu_brand, this.bandwidth);
+  ClientInfo(
+      this.login, this.device_id, this.os, this.cpu_brand, this.bandwidth);
 
   factory ClientInfo.fromJson(Map<String, dynamic> json) {
     final login = json['login'];
+    final device_id = json['device_id'];
     final os = OperationSystemInfo.fromJson(json['os']);
     final cpu_brand = json['cpu_brand'];
     final bandwidth = json['bandwidth'];
-    return ClientInfo(login, os, cpu_brand, bandwidth);
+    return ClientInfo(login, device_id, os, cpu_brand, bandwidth);
   }
 
   Map<String, dynamic> toJson() => {
         'login': login,
+        'device_id': device_id,
         'os': os.toJson(),
         'cpu_brand': cpu_brand,
         'bandwidth': bandwidth
