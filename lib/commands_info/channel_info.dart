91a145a668c3a78fc4e2ab3a4328d7bbf52ef31a
topilski <atopilski@gmail.com>
2019-10-04 17:02:19 +0300

    Preview and Description

--- lib/commands_info/channel_info.dart
+++ lib/commands_info/channel_info.dart
@@ -5,22 +5,27 @@ class ChannelInfo {
   final int type;
   final int stream_type;
   final String group;
+  final String description;
+  final String preview_icon;
   final EpgInfo epg;
   final bool video;
   final bool audio;
 
-  ChannelInfo(this.id, this.type, this.stream_type, this.group, this.epg,
-      this.video, this.audio);
+  ChannelInfo(this.id, this.type, this.stream_type, this.group,
+      this.description, this.preview_icon, this.epg, this.video, this.audio);
 
   factory ChannelInfo.fromJson(Map<String, dynamic> json) {
     final id = json['id'];
     final type = json['type'];
     final stream_type = json['stream_type'];
     final group = json['group'];
+    final description = json['description'];
+    final preview_icon = json['preview_icon'];
     final epg = EpgInfo.fromJson(json['epg']);
     final video = json['video'];
     final audio = json['audio'];
-    return ChannelInfo(id, type, stream_type, group, epg, video, audio);
+    return ChannelInfo(id, type, stream_type, group, description, preview_icon,
+        epg, video, audio);
   }
 
   Map<String, dynamic> toJson() => {
@@ -28,6 +33,8 @@ class ChannelInfo {
         'type': type,
         'stream_type': stream_type,
         'group': group,
+        'description': description,
+        'preview_icon': preview_icon,
         'epg': epg.toJson(),
         'video': video,
         'audio': audio
