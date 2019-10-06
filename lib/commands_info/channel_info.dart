de56c7b743a3e040f15830ecd90d5568f041ef6f
topilski <atopilski@gmail.com>
2019-10-06 06:31:51 +0300

    Enum for cheannel info

--- lib/commands_info/channel_info.dart
+++ lib/commands_info/channel_info.dart
@@ -1,9 +1,60 @@
 import 'package:fastotv_dart/commands_info/epg_info.dart';
 
+enum Type {
+  PUBLIC,
+  PRIVATE,
+}
+
+enum StreamType {
+  PROXY,
+  RELAY,
+  ENCODE,
+  TIMESHIFT_PLAYER,
+  TIMESHIFT_RECORDER,
+  CATCHUP,
+  TEST_LIFE,
+  VOD_RELAY,
+  VOD_ENCODE,
+  COD_RELAY,
+  COD_ENCODE,
+}
+
+Type Type2Enum(int number) {
+  if (number == 0) {
+    return Type.PUBLIC;
+  }
+  return Type.PRIVATE;
+}
+
+StreamType StreamType2Enum(int number) {
+  if (number == 0) {
+    return StreamType.PROXY;
+  } else if (number == 1) {
+    return StreamType.RELAY;
+  } else if (number == 2) {
+    return StreamType.ENCODE;
+  } else if (number == 3) {
+    return StreamType.TIMESHIFT_PLAYER;
+  } else if (number == 4) {
+    return StreamType.TIMESHIFT_RECORDER;
+  } else if (number == 5) {
+    return StreamType.CATCHUP;
+  } else if (number == 6) {
+    return StreamType.TEST_LIFE;
+  } else if (number == 7) {
+    return StreamType.VOD_RELAY;
+  } else if (number == 8) {
+    return StreamType.VOD_ENCODE;
+  } else if (number == 9) {
+    return StreamType.COD_RELAY;
+  }
+  return StreamType.COD_ENCODE;
+}
+
 class ChannelInfo {
   final String id;
-  final int type;
-  final int stream_type;
+  final Type type;
+  final StreamType stream_type;
   final String group;
   final String description;
   final String preview_icon;
@@ -16,8 +67,8 @@ class ChannelInfo {
 
   factory ChannelInfo.fromJson(Map<String, dynamic> json) {
     final id = json['id'];
-    final type = json['type'];
-    final stream_type = json['stream_type'];
+    final type = Type2Enum(json['type']);
+    final stream_type = StreamType2Enum(json['stream_type']);
     final group = json['group'];
     final description = json['description'];
     final preview_icon = json['preview_icon'];
