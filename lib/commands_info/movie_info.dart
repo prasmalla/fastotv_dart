d0eab358ff89d1b5a125f686d889fb6cebab7566
Vadim <44135514+vodemn@users.noreply.github.com>
2019-10-11 18:52:43 +0300

    Update movie_info.dart (#4)

--- lib/commands_info/movie_info.dart
+++ lib/commands_info/movie_info.dart
@@ -1,15 +1,21 @@
 class MovieInfo {
   final List<String> urls;
   final String description;
+  final String display_name;
   final String preview_icon;
 
-  MovieInfo(this.urls, this.description, this.preview_icon);
+  MovieInfo(this.urls, this.description, this.display_name, this.preview_icon);
 
   MovieInfo.fromJson(Map<String, dynamic> json)
       : urls = json['urls'],
         description = json['description'],
+        display_name = json['display_name'],
         preview_icon = json['preview_icon'] {}
 
-  Map<String, dynamic> toJson() =>
-      {'urls': urls, 'description': description, 'preview_icon': preview_icon};
+  Map<String, dynamic> toJson() => {
+        'urls': urls,
+        'description': description,
+        'display_name': display_name,
+        'preview_icon': preview_icon
+      };
 }
