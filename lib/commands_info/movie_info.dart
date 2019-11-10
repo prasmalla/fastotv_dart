814584a83e65cfe01d7ceb937b96dfe912d2ced1
topilski <atopilski@gmail.com>
2019-11-10 18:37:56 +0300

    Update Movie fields

--- lib/commands_info/movie_info.dart
+++ lib/commands_info/movie_info.dart
@@ -12,9 +12,23 @@ class MovieInfo {
   final String description;
   final String display_name;
   final String preview_icon;
+  final String trailer_url;
+  final double user_score;
+  final int prime_date;
+  final String country;
+  final int duration;
   final MovieType type;
 
-  MovieInfo(this.urls, this.description, this.display_name, this.preview_icon,
+  MovieInfo(
+      this.urls,
+      this.description,
+      this.display_name,
+      this.preview_icon,
+      final this.trailer_url,
+      this.user_score,
+      this.prime_date,
+      this.country,
+      this.duration,
       this.type);
 
   factory MovieInfo.fromJson(Map<String, dynamic> json) {
@@ -22,16 +36,26 @@ class MovieInfo {
     final description = json['description'];
     final display_name = json['display_name'];
     final preview_icon = json['preview_icon'];
+    final trailer_url = json['trailer_url'];
+    final user_score = json['user_score'];
+    final prime_date = json['prime_date'];
+    final country = json['country'];
+    final duration = json['duration'];
     final type = Type2Enum(json['type']);
-    return MovieInfo(urls, description, display_name, preview_icon, type);
+    return MovieInfo(urls, description, display_name, preview_icon, trailer_url,
+        user_score, prime_date, country, duration, type);
   }
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'urls': urls,
         'description': description,
         'display_name': display_name,
         'preview_icon': preview_icon,
+        'trailer_url': trailer_url,
+        'user_score': user_score,
+        'prime_date': prime_date,
+        'country': country,
+        'duration': duration,
         'type': type
       };
 }
