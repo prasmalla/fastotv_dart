59185e8f579e7f9418f4f9247aec02e6bb25c2e0
topilski <atopilski@gmail.com>
2019-12-05 16:31:46 +0300

    Catchup info

---
+++ lib/commands_info/catchup_info.dart
@@ -0,0 +1,6 @@
+import 'package:fastotv_dart/commands_info/channel_info.dart';
+
+class CatchupInfo extends ChannelInfo {
+  CatchupInfo(id, group, epg, video, audio)
+      : super(id, group, epg, video, audio);
+}
