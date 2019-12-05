655b63ff2ce57a685e9c8124c05bc0af2fdb2879
topilski <atopilski@gmail.com>
2019-12-05 17:07:11 +0300

    Catchup parse functions

--- lib/commands_info/catchup_info.dart
+++ lib/commands_info/catchup_info.dart
@@ -3,4 +3,13 @@ import 'package:fastotv_dart/commands_info/channel_info.dart';
 class CatchupInfo extends ChannelInfo {
   CatchupInfo(id, group, epg, video, audio)
       : super(id, group, epg, video, audio);
+
+  factory CatchupInfo.fromJson(Map<String, dynamic> json) {
+    final base = ChannelInfo.fromJson(json);
+    return CatchupInfo(base.id, base.group, epg, base.video, base.audio);
+  }
+
+  Map<String, dynamic> toJson() {
+    return super.toJson();
+  }
 }
