a143bc1adb445cb8fc0598194b00ecbb3b4d417a
topilski <atopilski@gmail.com>
2019-12-07 08:32:59 +0300

    Exp date

--- lib/commands_info/auth_info.dart
+++ lib/commands_info/auth_info.dart
@@ -2,20 +2,25 @@ import 'package:fastotv_dart/commands_info/login_info.dart';
 
 class AuthInfo extends LoginInfo {
   static const DEVICE_ID_FIELD = 'device_id';
+  static const EXPIRED_DATE_FIELD = 'exp_date';
 
+  final int exp_date;
   final String device_id;
 
-  AuthInfo(login, password, this.device_id) : super(login, password);
+  AuthInfo(login, password, this.exp_date, this.device_id)
+      : super(login, password);
 
   factory AuthInfo.fromJson(Map<String, dynamic> json) {
     final login = LoginInfo.fromJson(json);
+    final exp_date = json[EXPIRED_DATE_FIELD];
     final device_id = json[DEVICE_ID_FIELD];
-    return AuthInfo(login.login, login.password, device_id);
+    return AuthInfo(login.login, login.password, exp_date, device_id);
   }
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> login = super.toJson();
     login[DEVICE_ID_FIELD] = device_id;
+    login[EXPIRED_DATE_FIELD] = exp_date;
     return login;
   }
 }
