e12c2ec265ad1f52e8257bd292c6abfb907535d4
Vadim <44135514+vodemn@users.noreply.github.com>
2020-08-03 05:11:36 +0300

    Programme description (#22)
    * view_count
    
    * Meta
    
    * meta fix
    
    * Client content request
    
    * Description
    
    * Category

--- lib/commands_info/programme_info.dart
+++ lib/commands_info/programme_info.dart
@@ -1,3 +1,5 @@
+import 'package:quiver/core.dart';
+
 String _twoDigits(int n) {
   if (n >= 10) {
     return "$n";
@@ -10,13 +12,19 @@ class ProgrammeInfo {
   static const START_FIELD = 'start';
   static const STOP_FIELD = 'stop';
   static const TITLE_FIELD = 'title';
+  static const CATEGORY_FIELD = 'category';
+  static const DESCRIPTION_FIELD = 'description';
 
   final String channel;
   final int start;
   final int stop;
   final String title;
+  Optional<String> category = Optional<String>.absent();
+  Optional<String> description = Optional<String>.absent();
 
-  ProgrammeInfo(this.channel, this.start, this.stop, this.title);
+  ProgrammeInfo(this.channel, this.start, this.stop, this.title, String cat, String desc)
+      : category = _getOptional(cat),
+        description = _getOptional(desc);
 
   factory ProgrammeInfo.fromJson(Map<String, dynamic> json) {
     if (json == null) {
@@ -27,11 +35,25 @@ class ProgrammeInfo {
     final start = json[START_FIELD];
     final stop = json[STOP_FIELD];
     final title = json[TITLE_FIELD];
-    return ProgrammeInfo(channel, start, stop, title);
+    final category = json[CATEGORY_FIELD];
+    final desc = json[DESCRIPTION_FIELD];
+    return ProgrammeInfo(channel, start, stop, title, category, desc);
   }
 
   Map<String, dynamic> toJson() {
-    return {CHANNEL_FIELD: channel, START_FIELD: start, STOP_FIELD: stop, TITLE_FIELD: title};
+    Map<String, dynamic> result = {
+      CHANNEL_FIELD: channel,
+      START_FIELD: start,
+      STOP_FIELD: stop,
+      TITLE_FIELD: title
+    };
+    if (category.isPresent) {
+      result[CATEGORY_FIELD] = category.value;
+    }
+    if (description.isPresent) {
+      result[DESCRIPTION_FIELD] = description.value;
+    }
+    return result;
   }
 
   String getStart(Duration timeZoneOffset) {
@@ -58,5 +80,13 @@ class ProgrammeInfo {
     final diff = stopTime - startTime;
     final twoDigitMinutes = _twoDigits(diff.inMinutes.remainder(60));
     return '${_twoDigits(diff.inHours)}:$twoDigitMinutes';
+  }
+
+  static Optional<String> _getOptional(String desc) {
+    if (desc != null) {
+      return Optional<String>.fromNullable(desc);
+    }
+
+    return Optional<String>.absent();
   }
 }
